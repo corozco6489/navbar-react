442402958bde8fff109c00e2294d8a7e4108c566
carlos <orozcoalexander1996@gmail.com>
2021-12-20 16:43:52 -0500

    chmages

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
